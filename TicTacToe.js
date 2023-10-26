f97cbc843fb1c343b757756e5cf3611450b56eb4
Pierre GAMEN <pierre.gamen@gmail.com>
2023-10-26 13:10:55 +0200

    feat : button js : new css style

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,3 +1,18 @@
-function croix () {
+function spooky(){
+
+    const buttonTC = document.querySelector(".buttonTC");
     
-}
+    const link = document.getElementById("myLink");
+
+    if(buttonTC.innerHTML === "NEON") {
+        buttonTC.innerHTML = "SPOOKY"
+    } else {
+        buttonTC.innerHTML = "NEON"
+    }
+
+    if (link.getAttribute("href") === "TicTacToe.css") {
+    link.setAttribute("href", "spooky.css");
+  } else {
+    link.setAttribute("href", "TicTacToe.css");
+  }
+};
